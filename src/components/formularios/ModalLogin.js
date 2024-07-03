0d45289a1649cac5fdf6e308bb9b743ae62a0ba7
JesussBaa <jesuss.barrazaa95@gmail.com>
2024-07-03 00:39:11 -0600

    Modificación a etiqueta div ¿Olvidaste la contraseña?

--- src/components/formularios/ModalLogin.js
+++ src/components/formularios/ModalLogin.js
@@ -66,7 +66,7 @@ export default function ModalLogin() {
                     <Form onSubmit={handleLoginSubmit}>
                         <EmailLogin/>
                         <br />
-                        <div className='p-4'>
+                        <div className='p-4 d-flex justify-content-center'>
                             <Link to='/lost'>¿Olvidaste la contraseña?</Link>
                         </div>
                         <Stack className='p-5' direction="vertical" gap={2}>
